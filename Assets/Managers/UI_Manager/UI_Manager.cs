c7d4454372a2b5cf2beae7ef050670d91420b1ea
Volkol23 <adriguiu97@gmail.com>
2023-11-30 00:42:19 +0100

    UI Manager + New character

--- Assets/Managers/UI_Manager/UI_Manager.cs
+++ Assets/Managers/UI_Manager/UI_Manager.cs
@@ -1,11 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_Manager : MonoBehaviour
 {
     public static UI_Manager _UI_MANAGER;
 
+
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject backgroundFade;
+    [SerializeField] private GameObject abilitys;
+    [SerializeField] private GameObject missionObjective;
+    [SerializeField] private GameObject currentRiver;
+
+    [SerializeField]
+    private Color fadeInColor;
+    [SerializeField]
+    private Color fadeOutColor;
+
+    private Image fade;
     private void Awake()
     {
         if(_UI_MANAGER != null && _UI_MANAGER != this)
@@ -16,6 +30,58 @@ public class UI_Manager : MonoBehaviour
         {
             _UI_MANAGER = this;
             DontDestroyOnLoad(_UI_MANAGER);
+
+            fade = backgroundFade.GetComponent<Image>();
         }
+    }
+
+    public void ActivatePause()
+    {
+        pauseMenu.SetActive(true);
+    }
+
+    public void ActivateAbilities()
+    {
+        abilitys.SetActive(true);
+    }
+
+    public void ActivateMissionObjective()
+    {
+        missionObjective.SetActive(true);
+    }
+
+    public void ActivateCurrentRiver ()
+    {
+        currentRiver.SetActive(true);
+    }
+
+    public void DeactivatePauseMenu()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    public void DeactivateAbilities()
+    {
+        abilitys.SetActive(false);
+    }
+
+    public void DeactivateMissionObjective()
+    {
+        missionObjective.SetActive(false);
+    }
+
+    public void DeactivateCurrentRiver()
+    {
+        currentRiver.SetActive(false);
+    }
+
+    public void FadeIn()
+    {
+        fade.CrossFadeColor(fadeInColor, 1f, true, true);
+    }
+
+    public void FadeOut()
+    {
+        fade.CrossFadeColor(fadeOutColor, 1f, true, true);
     }
 }
