21c5af4259d6306b8aa8a64263e72caafb9df812
Volkol23 <adriguiu97@gmail.com>
2024-01-25 15:33:17 +0100

    Level Tweaks

---
+++ Assets/Managers/UI_Manager/UI_Ability.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UI_Ability : MonoBehaviour
+{
+    [SerializeField] private Image abilityImage;
+    [SerializeField] private TMP_Text abilityText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
